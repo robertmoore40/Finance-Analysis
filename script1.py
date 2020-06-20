ff8aab783d8595ceda2ced9d01f10e34132d328b
robertmoore40 <robertcmoore92@gmail.com>
2020-06-20 12:37:30 -0400

    added routes

--- script1.py
+++ script1.py
@@ -5,3 +5,11 @@
 from flask import Flask, render_template
 
 app=Flaske(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
