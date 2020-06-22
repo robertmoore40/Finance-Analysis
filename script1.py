2a8a11632b221438f05a63b825729ca40684fbe2
robertmoore40 <robertcmoore92@gmail.com>
2020-06-21 21:47:16 -0400

    added candlestick

--- script1.py
+++ script1.py
@@ -40,5 +40,28 @@ def plot():
     df["Middle"]=(df.Open+df.Close)/2
     df["Height"]=abs(df.Close-df.Open)
 
+    p=figure(x_axis_type='datetime', width=1000, height=300, sizing_mode="scale_both")
+    p.title.text="Candlestick Chart"
+    p.grid.grid_line_alpha=0.3
+
+    hours_12=12*60*60*1000
+
+    p.segment(df.index, df.High, df.index, df.Low, color="Black")
+
+    p.rect(df.index[df.Status=="Increase"],df.Middle[df.Status=="Increase"],
+           hours_12, df.Height[df.Status=="Increase"],fill_color="#CCFFFF",line_color="black")
+
+    p.rect(df.index[df.Status=="Decrease"],df.Middle[df.Status=="Decrease"],
+           hours_12, df.Height[df.Status=="Decrease"],fill_color="#FF3333",line_color="black")
+
+    script1, div1 = components(p)
+    cdn_js=CDN.js_files[0]
+    cdn_css=CDN.css_files[0]
+    return render_template("plot.html",
+    script1=script1,
+    div1=div1,
+    cdn_css=cdn_css,
+    cdn_js=cdn_js )
+
 if __name__=="__main__":
     app.run(debug=True)
